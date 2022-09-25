391537474ee4ff0b52ed9a91fdc698dc9564e925
parthmaru <199emperor@gmail.com>
2022-09-25 19:23:24 +0530

    responsive design fix

--- src/components/BlogDetail/BlogCards/BlogCards.jsx
+++ src/components/BlogDetail/BlogCards/BlogCards.jsx
@@ -3,8 +3,6 @@ import "./BlogCards.css";
 import testImg from "../../../images/blog1.jpg";
 
 const BlogCards = ({ title, description, category }) => {
-  console.log("title", title);
-
   return (
     <div className="blog_card_container">
       <div className="blog_card_img_container">
